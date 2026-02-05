37c9a161ea916fa4ec00cd37e555cdd8dacab593
FelipeButhay <felipebuthay@gmail.com>
2026-02-05 11:27:23 -0300

    Create repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+export default function App() {
+
+	
+	return (
+		<h1>AAAAAAAAA</h1>
+	)
+} 
